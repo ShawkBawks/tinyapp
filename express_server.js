c05f88cf3d9da60e92043f01257aab7ca6a1250d
Logan Roberts <lroberts77@gmail.com>
2019-09-30 23:47:39 +0000

    Set up random string generator

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -8,6 +10,20 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  Math.random().toString(36).slice(-6);
+return Math.random().toString(36).slice(-6);
+};
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
